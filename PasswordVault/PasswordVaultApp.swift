da1a9ff933e1859d461450e076d647f544f8e91f
TechJonesAi <deecos002@hotmail.com>
2025-12-03 00:07:48 +0000

    Initial Commit

---
+++ PasswordVault/PasswordVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordVaultApp.swift
+//  PasswordVault
+//
+//  Created by Darren Jones on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct PasswordVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
